b9878cef9b545ae5196483085c50cf23149d99b4
osboxes.org <bchen36@illinois.edu>
2018-02-08 22:42:03 +0000

    good checkpoint. preprocessing mostly done, now can get to work

--- nc_average.c
+++ nc_average.c
@@ -1,11 +1,13 @@
 #include "nc_wrapper.h"
-#include "math.h"
+#include <math.h>
+#include <string.h>
 
 int disable_clobber;
 int enable_verbose;
 int input_dir_flag;
 int input_file_flag;
 char input_file[256];
+
 
 /* Special indexing variables */
 extern int VAR_ID_TIME, VAR_ID_LVL, VAR_ID_LAT, VAR_ID_LON, VAR_ID_SPECIAL;
@@ -20,13 +22,52 @@ extern char FILE_CUR[];
 extern char FILE_NEXT[];
 extern char COPY[];
 
+extern size_t TIME_STRIDE;
+extern size_t LVL_STRIDE;
+extern size_t LAT_STRIDE;
+
+
 double INPUT_LAT, INPUT_LON;
 int INPUT_YEAR, INPUT_MONTH, INPUT_HOUR;
+
+Variable *vars;
+Dimension *dims;
+
+int file_id, num_vars, num_dims;
+
+/* 
+ * Find idx of last period, NULL terminate, then find second to last and return the year
+ * year - extracted value from file_name
+ * file_name - input filename
+ */
+int extract_year(int* year, const char* file_name) {
+    int i = 0;
+    const char period = '.';
+    char *copy, *last_per, *sec_per;
+    
+    // Create copy of file_name for extraction
+    copy = (char *) malloc(sizeof(char) * (strlen(file_name) + 1));
+    strcpy(copy, file_name);
+
+    if (strlen(copy) > 0) {
+        last_per = strrchr(copy, period); 
+        if ((last_per != NULL) && (last_per > 0)) {
+            copy[last_per - copy] = '\0';
+            sec_per = strrchr(copy, period);
+
+            if ((sec_per != NULL) && (sec_per > 0)) {
+                *year = atoi(sec_per + 1); 
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
 
 /* Return days in particular month (1-12) for that year. */
 int days_in_month(int y, int m) {
 	if (m == 2) {
-		if ((y % 4 == 0 && y % 4 != 0) || y % 400 == 0)
+		if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
 			return 29;
 		return 28;
 	}
@@ -36,25 +77,57 @@ int days_in_month(int y, int m) {
 	return 31;
 }
 
-/*
- * [start_m, end_m] - start to end month range, specified with values 1-12. 
- * if start_m == 0: process whole year
- */
-int collect_range(int year, int start_m, int end_m) {
-	if (start_m == 0) {
-		start_m = 1;
-		end_m = 12;
-	}
+/* Prepares for processing */
+int calibrate(int year, int month, double lat, double lon, int hour, const char* input_file) {
+    int total_days, start_m, end_m;
 
-	// Collect number of time units (4 * total_days) for indexing into other files (1460, 1464 remember)
-	// Need 'start_offset_idx' and either 'count' or 'end' idx 
-	for (int i = start_m; i <= end_m; i++) {
-		printf("i %d\n", i);
-	}
+    if (month == 0) {
+        start_m = 1;
+        end_m = 12;
+    } else {
+        start_m = month;
+        end_m = month + 2;
+    }
+
+    total_days = 0;
+    for (int i = start_m; i <= end_m; i++) {
+        total_days += days_in_month(year, i);
+    }
+    
+    ___nc_open(input_file, &file_id);
+
+    nc_inq(file_id, &num_dims, &num_vars, NULL, NULL);
+
+    /* Output Dimension Information */
+    dims = (Dimension *) malloc(num_dims * sizeof(Dimension));
+    for (int i = 0; i < num_dims; i++) {
+        ___nc_inq_dim(file_id, i, &dims[i]);
+    }
+
+    /* Output Variable Information */
+    vars = (Variable *) malloc(num_vars * sizeof(Variable));
+    for (int i = 0; i < num_vars; i++) {
+        ___nc_inq_var(file_id, i, &vars[i], dims);
+    }
+
+    printf("Loading variable data\n");
+    for (int i = 0; i < num_vars; i++) {
+        ___nc_get_var_array(file_id, i, &vars[i], dims);
+    }
+
+    TIME_STRIDE = dims[DIM_ID_LON].length * dims[DIM_ID_LAT].length * dims[DIM_ID_LVL].length;
+    LVL_STRIDE  = dims[DIM_ID_LON].length * dims[DIM_ID_LAT].length;
+    LAT_STRIDE  = dims[DIM_ID_LON].length;
+    printf("WORK PELASE\n");
+
+    return 0;
 }
 
 int process_average_arguments(int argc, char* argv[]) {
     int opt;
+
+    INPUT_LAT = INPUT_LON = -1000.0;
+    INPUT_YEAR = INPUT_MONTH = INPUT_HOUR = -1;
     while ((opt = getopt(argc, argv, "r:c:y:m:h:i:f:o:p:s:dv")) != -1) {
         switch(opt) {
             /* Primary user arguments prefixed with 'INPUT_' */
@@ -64,9 +137,6 @@ int process_average_arguments(int argc, char* argv[]) {
             case 'c': /* Longitude */
                 INPUT_LON = atof(optarg);
                 break;
-            case 'y': /* Year */
-                INPUT_YEAR = atoi(optarg);
-                break;
             case 'm': /* Month */
                 INPUT_MONTH = atoi(optarg);
                 break;
@@ -74,10 +144,10 @@ int process_average_arguments(int argc, char* argv[]) {
                 INPUT_HOUR = atoi(optarg);
                 break;
             /* Additional program args */
-            case 'i':
-                strcpy(input_dir, optarg);
-                input_dir_flag = 1;
-                break;
+            // case 'i':
+            //     strcpy(input_dir, optarg);
+            //     input_dir_flag = 1;
+            //     break;
             case 'f':
                 strcpy(input_file, optarg);
                 input_file_flag = 1;
@@ -103,10 +173,54 @@ int process_average_arguments(int argc, char* argv[]) {
         }
     }
 
-    /* NCAR/NCEP Reanalysis  is [-90.0, 90.0] degrees */
+    /* NCAR/NCEP Reanalysis - Latitude: [-90.0, 90.0]; Longitude: [0, 357.5] degrees */
     if (INPUT_LAT < -90.0 || INPUT_LAT > 90.0 || fmod(INPUT_LAT, 2.5) != 0.0) {
-        fprintf(stderr, "Latitude not valid\n");
+        fprintf(stderr, "Latitude invalid (%f). \
+            Accepts values in [-90.0, 90.0] range as multiples of 2.5 degrees\n", INPUT_LAT);
+        return 1;
     }
+
+    if (INPUT_LON < 0.0 || INPUT_LON > 357.5 || fmod(INPUT_LON, 2.5) != 0.0) {
+        fprintf(stderr, "Longitude invalid (%f). \
+            Accepts values in [0, 357.5] range as multiples of 2.5 degrees\n", INPUT_LON);
+        return 1;
+    }
+
+    /* 13 values supported; 1-12 corresponding to the months, and 0 to process the whole year */
+    if (INPUT_MONTH < 0 || INPUT_MONTH > 12) {
+        fprintf(stderr, "Month invalid (%d). \
+            Must be within [0, 12], where 1:January, 2:February, ..., and 0 for entire year\n", INPUT_MONTH);
+        return 1;
+    }
+
+    /* 24 hour clock system */
+    if (INPUT_HOUR < 0 || INPUT_HOUR > 24) {
+        fprintf(stderr, "Hour invalid (%d). Must be within [0, 24] on 24-hour clock system\n", INPUT_HOUR);
+        return 1;
+    }
+
+    if (input_file_flag != 1 || extract_year(&INPUT_YEAR, input_file)) {
+        fprintf(stderr, "Failed to extract year from input file\n");
+        return 1;
+    }
+
+    /* Ensure a 4 digit realistic number */
+    if (INPUT_YEAR < 1800 || INPUT_YEAR > 2050) {
+        fprintf(stderr, "Year invalid (%d). Accepts values in [1800, 2050] (arbitrary end year)\n", INPUT_YEAR);
+        return 1;
+    }
+
+    printf("Program proceeding with:\n");
+    printf("\tLatitutde: %f\n", INPUT_LAT);
+    printf("\tLongitude: %f\n", INPUT_LON);
+    printf("\tYear | Month | Hour: %d | %d | %d\n", INPUT_YEAR, INPUT_MONTH, INPUT_HOUR);
+    printf("\tDisable clobbering = %d\n", disable_clobber);
+    printf("\tInput directory = %s\n", input_dir);
+    printf("\tOutput directory = %s\n", output_dir);
+    printf("\tOutput file prefix = %s\n", prefix);
+    printf("\tOutput file suffix = %s\n", suffix);
+
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
@@ -120,18 +234,22 @@ int main(int argc, char* argv[]) {
     if (process_average_arguments(argc, argv)) {
         printf("Program usage: %s\n", argv[0]); 
         printf("\t-r [latitude] -c [longitude] -y [year] -m [month] -h [hour (24hr)]\n");
-        printf("\t-i [input directory] -d [delete flag; no args]\n");
+        printf("\t-i [input directory] -f [input file] -d [delete flag; no args]\n");
         printf("\t-o [output directory] -p [output FILE_CUR prefix] -s [output FILE_CUR suffix] -v [verbose; no args]\n");
         exit(1);
     }
 
-    printf("arg 0 %s arg 1 %s\n", argv[0], argv[1]);
+    if (calibrate(INPUT_YEAR, INPUT_MONTH, INPUT_LON, INPUT_LAT, INPUT_HOUR, input_file)) {
+        printf("Gather data function failed\n");
+        exit(1);
+    }
 
-    collect_range(2013, 0, 0);
+    // int days = days_in_month(INPUT_YEAR, INPUT_MONTH);
+    // printf("%d/%d has %d days\n", INPUT_MONTH, INPUT_YEAR, days);
 
 
     printf("Averaging program completed successfully.\n");
-
+    return 0;
 }
 
 
